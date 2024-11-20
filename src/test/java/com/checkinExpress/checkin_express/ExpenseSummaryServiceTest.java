b6ed0095e2a077e1bb942e8dc08fd0e31df2e80a
HenriqueMartelini <henriquezmartelini@hotmail.com>
2024-11-20 10:49:52 -0300

    Server add

--- src/test/java/com/checkinExpress/checkin_express/ExpenseSummaryServiceTest.java
+++ src/test/java/com/checkinExpress/checkin_express/ExpenseSummaryServiceTest.java
@@ -31,30 +31,23 @@ public class ExpenseSummaryServiceTest {
 
     @Test
     public void testCalculateTotalExpense() {
-        // Criar o ExpenseSummary passando a lista de despesas diretamente no construtor
         ExpenseSummary expenseSummary = new ExpenseSummary(Collections.singletonList(expense), 100.0);  // Passando as despesas e total
 
-        // Calcular o total
         double total = expenseSummaryService.calculateTotalExpense(expenseSummary);
 
-        // Verificar se o total é 100.0
         assertEquals(100.0, total);
     }
 
     @Test
     public void testGetExpenseSummary() {
-        // Criar a reserva com uma lista de despesas
         Booking booking = new Booking();
         booking.setId("1");
         booking.setExpenses(Collections.singletonList(expense));
 
-        // Simular a busca no repositório
         Mockito.when(bookingRepository.findById("1")).thenReturn(java.util.Optional.of(booking));
 
-        // Chamar o método de serviço para obter o ExpenseSummary
         ExpenseSummary result = expenseSummaryService.getExpenseSummary("1");
 
-        // Verificar se o resultado não é nulo e o total das despesas
         assertNotNull(result);
         assertEquals(100.0, result.getTotal());
     }
