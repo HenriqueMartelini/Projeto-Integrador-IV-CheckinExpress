dd72e8bacb4bd84feb42b92b35bc1d7f52b84da4
HenriqueMartelini <henriquezmartelini@hotmail.com>
2024-11-22 16:38:59 -0300

    Ajustes

--- src/main/java/com/checkinExpress/checkin_express/CheckinExpressApplication.java
+++ src/main/java/com/checkinExpress/checkin_express/CheckinExpressApplication.java
@@ -11,9 +11,20 @@ public class CheckinExpressApplication {
 		// Carrega as variáveis do arquivo .env
 		Dotenv dotenv = Dotenv.configure().load();
 
-		// Configura as variáveis como variáveis de sistema
-		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
-		System.setProperty("MONGODB_DATABASE", dotenv.get("MONGODB_DATABASE"));
+		// Recupera as variáveis de ambiente
+		String mongodbUrl = dotenv.get("DATABASE_URL");
+		String mongodbUser = dotenv.get("DATABASE_USER");
+		String mongodbPassword = dotenv.get("DATABASE_PASSWORD");
+
+		// Verifica se as variáveis estão carregadas corretamente
+		if (mongodbUrl == null || mongodbUser == null || mongodbPassword == null) {
+			throw new IllegalStateException("As variáveis de ambiente não foram encontradas no arquivo .env.");
+		}
+
+		// Configura as variáveis como variáveis de sistema (se necessário)
+		System.setProperty("DATABASE_URL", mongodbUrl);
+		System.setProperty("DATABASE_USER", mongodbUser);
+		System.setProperty("DATABASE_PASSWORD", mongodbPassword);
 
 		SpringApplication.run(CheckinExpressApplication.class, args);
 	}
